4de821cc256ef86362e0baab1ad761d640c47150
Migue Martelo <miguemartelo1031@gmail.com>
2023-04-04 12:34:46 -0500

    refactor: use getStaticProps in the home page and usign with react-query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import { NextPage } from 'next';
 import Head from 'next/head';
+import { QueryClient, dehydrate } from '@tanstack/react-query';
 
 import { useMovies } from '@/hooks';
 import { MovieList } from '@/components';
+import { getMovies } from '@/services';
 
-export default function Home() {
+const Home: NextPage = () => {
   const { data } = useMovies();
 
   return (
@@ -13,8 +16,22 @@ export default function Home() {
       </Head>
       <main className='max-w-5xl mx-auto'>
         <h1 className='text-4xl font-bold text-center'>Movie Inc</h1>
-        <MovieList movies={data?.results || []} />
+        <MovieList movies={data?.results} />
       </main>
     </>
   );
+};
+
+export default Home;
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(['movies'], getMovies);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+    revalidate: 60 * 60 * 24, // 24 hours because the data is static and doesn't change often
+  };
 }
