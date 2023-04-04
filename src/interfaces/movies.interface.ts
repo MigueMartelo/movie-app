289692d77c4d3922b4cf8db11a65de22a5baccd1
Migue Martelo <miguemartelo1031@gmail.com>
2023-04-03 22:59:17 -0500

    feat: add MovieView component and some interfaces

--- src/interfaces/movies.interface.ts
+++ src/interfaces/movies.interface.ts
@@ -16,3 +16,23 @@ export interface Movie {
   vote_average: number;
   vote_count: number;
 }
+
+export interface IGenre {
+  id: number;
+  name: string;
+}
+
+export interface ICast {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+}
--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -1,30 +1,7 @@
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import Image from 'next/image';
 import { useMovie, useMovieCredits } from '@/hooks';
-import config from '@/config';
-
-const { IMAGE_URL } = config;
-
-interface IGenre {
-  id: number;
-  name: string;
-}
-
-interface ICast {
-  adult: boolean;
-  gender: number;
-  id: number;
-  known_for_department: string;
-  name: string;
-  original_name: string;
-  popularity: number;
-  profile_path: string;
-  cast_id: number;
-  character: string;
-  credit_id: string;
-  order: number;
-}
+import { MovieView } from '@/components/MovieView/MovieView';
 
 const MoviePage = () => {
   const router = useRouter();
@@ -42,48 +19,7 @@ const MoviePage = () => {
         <h1 className='text-3xl font-bold text-center'>Movie Inc</h1>
         <div className='my-4'>
           {movie && (
-            <>
-              <div className='flex justify-between font-bold mb-2'>
-                <h2 className='text-2xl'>{movie.title}</h2>
-                <span>Release: {movie.release_date}</span>
-              </div>
-              <div className='flex gap-4'>
-                <Image
-                  src={`${IMAGE_URL}${movie.poster_path}`}
-                  alt={movie.title}
-                  width={500}
-                  height={500}
-                />
-                <div>
-                  <p className='font-bold'>Overview:</p>
-                  <p>{movie.overview}</p>
-                  <div className='flex'>
-                    <span className='font-bold'>Genres:</span>
-                    {movie.genres.map((gen: IGenre) => (
-                      <span key={gen.name} className='mr-2'>
-                        {gen.name}
-                      </span>
-                    ))}
-                  </div>
-                  <div>
-                    <span className='font-bold'>Rating:</span>{' '}
-                    <span>{movie.vote_average.toFixed(1)}/10</span>
-                  </div>
-                  {movieCredits && (
-                    <div>
-                      <span className='font-bold'>Cast:</span>
-                      {movieCredits.cast.map((cast: ICast) => (
-                        <p key={cast.id}>
-                          <span className='mr-2'>
-                            {cast.name} as {cast.character}
-                          </span>
-                        </p>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              </div>
-            </>
+            <MovieView movie={movie} movieCredits={movieCredits?.cast} />
           )}
         </div>
         <button
